535824e99a0ecb81f3b9f687871667d02996ccbc
Matheus Moreira <micaelmodesto11@gmail.com>
2024-06-19 20:40:40 -0300

    FEATURE - Adicionando dependencias do projeto

---
+++ src/main/java/com/schoolsync/schoolsynchback/SchoolsynchbackApplication.java
@@ -0,0 +1,13 @@
+package com.schoolsync.schoolsynchback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolsynchbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolsynchbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolsync/schoolsynchback/SchoolsynchbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolsync.schoolsynchback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolsynchbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
